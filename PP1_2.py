7ec0446880584d64a5ebc7aaa8d12cc984642cb1
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-09-16 17:44:37 +0000

    finished assignment

--- PP1_2.py
+++ PP1_2.py
@@ -1,26 +1,52 @@
-
+'''
+    Title: Variables Assignment
+    Author: Andy Chow
+    Date Created: Sept 16, 2024
+    Date Last Modified: Spet 16, 2024
+'''
 
 
 
 def q1():
+  name = "Alex"
+  print(name)
   #Write Assignment code here
 
 def q2():
+  numOne = 3
+  numTwo = 5
+  numThree = 2
+  print(numOne + numTwo + numThree)
   #Write Assignment code here
 
 def q3():
+  numFour = 21
+  numFive = 4
+  print(numFour // numFive)
   #Write Assignment code here
 
 def q4():
+  floatOne = 6.3
+  floatTwo = 3.0
+  print(floatOne / floatTwo)
   #Write Assignment code here
 
 def q5():
+  vari = "Hello World"
+  print(vari)
+  vari = "Goodbye World"
+  print(vari)
   #Write Assignment code here
 
 def q6():
+  variTwo = True
+  print (variTwo)
   #Write Assignment code here
 
 def q7():
+  numSix = 7
+  numSeven = 2
+  print(numSix % numSeven)
   #Write Assignment code here
 
   
